08eb345107b10ef7aeab8038fe38d1cb7a30949c
Tushar Bana <tbana@synopsys.com>
2026-06-11 16:46:26 +0530

    style: fix conf.py import order

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,11 +16,11 @@
 
 """Sphinx documentation configuration file."""
 
-from datetime import datetime
 import os
-from pathlib import Path
 import re
 import sys
+from datetime import datetime
+from pathlib import Path
 
 from ansys_sphinx_theme import ansys_favicon, get_version_match, pyansys_logo_black
 
